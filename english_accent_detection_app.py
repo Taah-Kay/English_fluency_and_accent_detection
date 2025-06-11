889abc699f65afbcf1f851af7a71a920545e9059
Ryan Kembo <95131904+Taah-Kay@users.noreply.github.com>
2025-06-11 23:33:27 +0200

    Update english_accent_detection_app.py
    added torchvision import

--- english_accent_detection_app.py
+++ english_accent_detection_app.py
@@ -6,6 +6,7 @@ import requests
 import tempfile
 import tempfile
 import torchaudio
+import torchvision
 from speechbrain.pretrained.interfaces import foreign_class
 
 def download_video_from_url(url):
