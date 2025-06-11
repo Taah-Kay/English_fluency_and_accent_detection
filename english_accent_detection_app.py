1b45296db3c186992118f058ed8b5396e708f89a
Ryan Kembo <95131904+Taah-Kay@users.noreply.github.com>
2025-06-12 00:31:14 +0200

    Update english_accent_detection_app.py

--- english_accent_detection_app.py
+++ english_accent_detection_app.py
@@ -80,14 +80,16 @@ video_path = None
 if option == "Upload video file":
     uploaded_video = st.file_uploader("Upload your video", type=["mp4", "mov", "avi", "mkv"])
     if uploaded_video is not None:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video:
-            temp_video.write(uploaded_video.read())
-            video_path = temp_video.name
+
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(video_file.read())
+            video_path = temp_file.name
+            
         '''temp_video_path = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")
         with open(temp_video_path.name, "wb") as f:
             f.write(uploaded_video.read())
         video_path = temp_video_path.name'''
-        st.success("Video uploaded successfully.")
+            st.success("Video uploaded successfully.")
 
 elif option == "Enter direct MP4 URL":
     video_url = st.text_input("Enter direct video URL (e.g., MP4 link)")
