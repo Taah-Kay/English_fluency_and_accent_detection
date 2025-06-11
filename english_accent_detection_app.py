9dcf1a441dabd67a66957e574d95c9ebdfb41e32
Ryan Kembo <95131904+Taah-Kay@users.noreply.github.com>
2025-06-11 23:17:24 +0200

    Update english_accent_detection_app.py

--- english_accent_detection_app.py
+++ english_accent_detection_app.py
@@ -6,7 +6,7 @@ import requests
 import tempfile
 import tempfile
 import torchaudio
-from speechbrain.inferece import foreign_class
+from speechbrain.inference import foreign_class
 
 def download_video_from_url(url):
     """
