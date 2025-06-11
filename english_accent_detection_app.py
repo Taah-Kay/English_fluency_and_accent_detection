e89307552ef9abe5280e47e514eece4fdf517632
Ryan Kembo <95131904+Taah-Kay@users.noreply.github.com>
2025-06-11 23:06:40 +0200

    Update english_accent_detection_app.py
    change from speechbrain.pretrained (deprecated) to speechbrain.inference

--- english_accent_detection_app.py
+++ english_accent_detection_app.py
@@ -6,7 +6,7 @@ import requests
 import tempfile
 import tempfile
 import torchaudio
-from speechbrain.pretrained.interfaces import foreign_class
+from speechbrain.interfaces import foreign_class
 
 def download_video_from_url(url):
     """
