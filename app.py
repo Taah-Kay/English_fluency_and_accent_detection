0d853a5b4cf0c4811588fb56fbbd9d89183d9fc0
Ryan Kembo <95131904+Taah-Kay@users.noreply.github.com>
2025-06-17 22:48:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from utils.accent_analysis import analyze_accent
 from utils.session_utils import initialize_session_state, display_memory_once, reset_session_state_except_model
 from models.custom_interface import CustomEncoderWav2vec2Classifier
 
-st.title("🎙️ English Accent Audio Detector")
+st.title("English Accent Audio Detector")
 
 # Initialize session state
 initialize_session_state()
@@ -27,7 +27,7 @@ if 'whisper' not in st.session_state:
 display_memory_once()
 
 # Reset state for a new analysis
-if st.button("🔄 Analyze new video"):
+if st.button("Analyze new video"):
     reset_session_state_except_model()
     st.rerun()
 
@@ -45,7 +45,7 @@ if option == "Upload video file":
         with open(temp_video_path.name, "wb") as f:
             f.write(uploaded_video.read())
         audio_path = trim_video(temp_video_path.name)
-        st.success("✅ Video uploaded successfully.")
+        st.success("Video uploaded successfully.")
         st.session_state.audio_path = audio_path
         
 
@@ -56,18 +56,18 @@ elif option == "Enter Video Url":
             audio_path = download_audio_as_wav(yt_url)
             audio_path = trim_audio(audio_path)
         if audio_path:
-            st.success("✅ Video downloaded successfully.")
+            st.success("Video downloaded successfully.")
             st.session_state.audio_path = audio_path
             
 
 # Transcription and Accent Analysis
 if st.session_state.audio_path and not st.session_state.transcription:
-    if st.button("🎧 Extract Audio"):
+    if st.button("Extract Audio"):
         st.session_state.audio_ready = True
         st.audio(st.session_state.audio_path, format='audio/wav')
         
         mem = psutil.virtual_memory()
-        st.write(f"🔍 Memory used: {mem.percent}%")    
+        st.write(f"Memory used: {mem.percent}%")    
         #Detect Language AND FILTER OUT NON-ENGLISH AUDIOS FOR ANALYSIS
         segments, info = st.session_state.whisper.transcribe(st.session_state.audio_path, beam_size=1)
             
@@ -76,34 +76,34 @@ if st.session_state.audio_path and not st.session_state.transcription:
                   
         if info.language != "en":
                     
-            st.error("❌ This video does not appear to be in English. Please provide a clear English video.")
+            st.error("This video does not appear to be in English. Please provide a clear English video.")
         else:    
             # Show transcription for audio
             with st.spinner("Transcribing audio..."):
                 st.markdown(" Transcript Preview")
                 st.markdown(st.session_state.transcription)
-                st.success("🎵 Audio extracted and ready for analysis!")
+                st.success("Audio extracted and ready for analysis!")
                 mem = psutil.virtual_memory()
-                st.write(f"🔍 Memory used: {mem.percent}%")
+                st.write(f"Memory used: {mem.percent}%")
 
        
 
 if st.session_state.transcription:
-    if st.button("🗣️ Analyze Accent"):
-        with st.spinner("🔍 Analyzing accent..."):
+    if st.button("Analyze Accent"):
+        with st.spinner("Analyzing accent..."):
             try:
                 mem = psutil.virtual_memory()
-                st.write(f"🔍 Memory used: {mem.percent}%")
+                st.write(f"Memory used: {mem.percent}%")
                 waveform, sample_rate = torchaudio.load(st.session_state.audio_path)
                 readable_accent, confidence = analyze_accent(waveform, sample_rate, st.session_state.classifier)
 
                 if readable_accent:
-                    st.success(f"✅ Accent Detected: **{readable_accent}**")
-                    st.info(f"📊 Confidence: {confidence}%")
+                    st.success(f"Accent Detected: **{readable_accent}**")
+                    st.info(f"Confidence: {confidence}%")
                    
                 else:
                     st.warning("Could not determine accent.")
                     
             except Exception as e:
-                st.error("❌ Failed to analyze accent.")
+                st.error("Failed to analyze accent.")
                 st.code(str(e))
